cbc7c31d9d180b82b00391147cc829e48f14ae69
Yaoxing <yaoxing.zhang@gmail.com>
2024-04-15 17:23:15 +0200

    + add code for performance test

--- performance-test/lib/stress-test.js
+++ performance-test/lib/stress-test.js
@@ -1,1 +1,55 @@
 var config = require("config");
+var ObjectId = require("mongodb").ObjectId;
+var getDB = require("./mongodb").getDB;
+
+var minId = ObjectId.createFromHexString(config.idRange.min);
+var maxId = ObjectId.createFromHexString(config.idRange.max);
+var minTime = minId.getTimestamp().getTime();
+var maxTime = maxId.getTimestamp().getTime();
+var time = maxTime - minTime;
+var numCandidates = config.numCandidates;
+var limit = config.limit;
+
+console.log(`_id range set to ${minId.toHexString()}~${maxId.toHexString()}`);
+
+module.exports = {
+    getRandomId: function() {
+        var timestamp = Math.floor((minTime + Math.random() * time) / 1000);
+        var hex = timestamp.toString(16) + "0000000000000000";
+        console.debug(`ObjectId generated: ${hex}`);
+
+        return ObjectId.createFromHexString(hex);
+    },
+    getEmbedding: async function(oid) {
+        var db = await getDB();
+        var doc = await db.collection("paper").findOne({
+            _id: {
+                "$gte": oid
+            }
+        });
+        var embedding = doc.embedding;
+        console.debug(`Document found: ${doc._id}`);
+        console.debug(`Embedding: [${embedding[1]},...,${embedding[embedding.length - 1]}]`);
+
+        return embedding;
+    },
+    vectorSearch: async function(embedding) {
+        var db = await getDB();
+        var pipeline = [{
+            $vectorSearch: {
+                index: "paper_embedding_search",
+                path: "embedding",
+                queryVector: embedding,
+                numCandidates: numCandidates,
+                limit: limit
+            }
+        }, {
+            $project: {
+                embedding: 0
+            }
+        }];
+        var result = await db.collection("paper").aggregate(pipeline).toArray();
+        console.debug(result);
+        return result;
+    }
+}
--- performance-test/routes/index.js
+++ performance-test/routes/index.js
@@ -1,9 +1,16 @@
+var getRandomId = require("../lib/stress-test").getRandomId;
+var getEmbedding = require("../lib/stress-test").getEmbedding;
+var vectorSearch = require("../lib/stress-test").vectorSearch;
 var express = require('express');
+var config = require("config");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/vector-search', function(req, res, next) {
-  res.json().status(200);
+router.get('/vector-search', async function(req, res, next) {
+  var oid = getRandomId();
+  var embedding = await getEmbedding(oid);
+  var result = await vectorSearch(embedding);
+  res.json(result).status(200);
 });
 
 module.exports = router;
